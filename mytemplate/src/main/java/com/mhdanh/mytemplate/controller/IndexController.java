154647b86c01ebcadb30cb46be53ccd7d2fcd1bc
maidanhcongtu@gmail.com <maidanhcongtu@gmail.com@27cc16b5-34d3-3ae7-c9fc-f95918a9f469>
2014-11-21 11:24:45 +0000

    add tld

--- mytemplate/src/main/java/com/mhdanh/mytemplate/controller/IndexController.java
+++ mytemplate/src/main/java/com/mhdanh/mytemplate/controller/IndexController.java
@@ -1,14 +1,21 @@
 package com.mhdanh.mytemplate.controller;
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
 	
+	private static final Logger LOGGER = Logger.getLogger(IndexController.class);
+	
 	@RequestMapping(value={"/index","/","/index/"})
-	public String index(){
+	public String index(Model model){
 		System.out.println("ok man");
+		LOGGER.debug("-----TEST LOGGER-----");
+		LOGGER.warn("-----TEST LOGGER-----");
+		model.addAttribute("ok", "hi every one");
 		return "indexIndex";
 	}
 }
--- mytemplate/src/main/webapp/view/pages/index/index.jsp
+++ mytemplate/src/main/webapp/view/pages/index/index.jsp
@@ -1,1 +1,8 @@
-hi every one
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<%@ taglib prefix="my" uri="/WEB-INF/my.tld" %>
+
+hi evero one
+<h1>${my.testok("mai huu danh")}</h1>
+why
+${ok}
+${dfdsaf}
