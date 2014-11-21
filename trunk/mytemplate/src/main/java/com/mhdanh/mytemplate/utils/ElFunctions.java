32dc48543d09b00653d5c01ce47d31ab937ca282
maidanhcongtu@gmail.com <maidanhcongtu@gmail.com@27cc16b5-34d3-3ae7-c9fc-f95918a9f469>
2014-11-21 11:24:45 +0000

    add tld

---
+++ trunk/mytemplate/src/main/java/com/mhdanh/mytemplate/utils/ElFunctions.java
@@ -0,0 +1,10 @@
+package com.mhdanh.mytemplate.utils;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ElFunctions {
+	public static String testok(String param){
+		return param;
+	}
+}
