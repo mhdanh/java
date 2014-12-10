eb2b7c34faad31fcda8f4a1864d015ec5e200a53
maidanhcongtu@gmail.com <maidanhcongtu@gmail.com@27cc16b5-34d3-3ae7-c9fc-f95918a9f469>
2014-12-10 15:40:35 +0000

    finish insert upload template

--- trunk/mytemplate/src/main/java/com/mhdanh/mytemplate/utility/Utility.java
+++ trunk/mytemplate/src/main/java/com/mhdanh/mytemplate/utility/Utility.java
@@ -7,13 +7,20 @@ import java.util.Locale;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
+
+import com.mhdanh.mytemplate.domain.Account;
+import com.mhdanh.mytemplate.service.AccountService;
 
 @Component
 public class Utility {
 
 	@Autowired
 	MessageSource messageSource;
+	@Autowired
+	AccountService accountService;
 	
 	public String getWebappPath() throws UnsupportedEncodingException {
 		String classPath = this.getClass().getClassLoader().getResource("")
@@ -23,7 +30,11 @@ public class Utility {
 		String webappPath = pathArr[0];
 		return webappPath;
 	}
-
+	/**
+	 * 
+	 * @return /view/html/
+	 * 
+	 */
 	public String getHtmlWebappPath() throws UnsupportedEncodingException {
 		return this.getWebappPath() + "/view/html/";
 	}
@@ -39,11 +50,33 @@ public class Utility {
 	 * @return
 	 */
 	public String convertTextInDatabaseToNormalText(String dataBaseText){
-		String [] slipedTextByDot = dataBaseText.split(".");
-		if(slipedTextByDot.length == 0){
+		String [] splipedTextByDot = dataBaseText.split("\\.");
+		if(splipedTextByDot.length == 0){
 			return null;
 		}
-		return slipedTextByDot[1].toLowerCase();
+		return splipedTextByDot[1].toLowerCase().replace('_', '-');
+	}
+	
+	/**
+	 * Get account of current logined
+	 * 
+	 * 
+	 */
+	public Account getUserLogined(){
+		 Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+	     String username = auth.getName();
+	     return accountService.getAccountByUsername(username);
+	}
+	/**
+	 * Exmple: first-template.zip to first-template 
+	 * 
+	 */
+	public String getNameWithouExtension(String fileName){
+		String [] splitedTextByDot = fileName.split("\\.");
+		if(splitedTextByDot.length == 0){
+			return null;
+		}
+		return splitedTextByDot[0];
 	}
 
 }
