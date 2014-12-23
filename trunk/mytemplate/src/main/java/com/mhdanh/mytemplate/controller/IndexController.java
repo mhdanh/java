f985261b9abe6e6b2786dc7fce861e42dff5dffe
maidanhcongtu@gmail.com <maidanhcongtu@gmail.com@27cc16b5-34d3-3ae7-c9fc-f95918a9f469>
2014-12-23 16:16:10 +0000

    begin index controller template

--- trunk/mytemplate/src/main/java/com/mhdanh/mytemplate/controller/IndexController.java
+++ trunk/mytemplate/src/main/java/com/mhdanh/mytemplate/controller/IndexController.java
@@ -1,25 +1,29 @@
 package com.mhdanh.mytemplate.controller;
 
+import java.util.List;
+
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.mhdanh.mytemplate.domain.UploadTemplate;
+import com.mhdanh.mytemplate.service.UploadTemplateService;
+
 @Controller
 public class IndexController {
 	
-	private static final Logger LOGGER = Logger.getLogger(IndexController.class);
+	private Logger logger = Logger.getLogger(IndexController.class);
+	
+	@Autowired
+	UploadTemplateService uploadTemplateService;
 	
 	@RequestMapping(value={"/index","/","/index/"})
 	public String index(Model model){
-		System.out.println("ok man");
-		LOGGER.debug("-----debug LOGGER-----");
-		LOGGER.warn("-----warn LOGGER-----");
-		LOGGER.info("-----info LOGGER-----");
-		LOGGER.error("-----error LOGGER-----");
-		LOGGER.fatal("-----fatal LOGGER-----");
-		model.addAttribute("ok", "hi every one");
+		List<UploadTemplate> templates = uploadTemplateService.getAll();
+		model.addAttribute("templates", templates);
 		return "/index";
 	}
 }
