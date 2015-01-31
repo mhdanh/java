08e9fcbee2c7c5828d4734e000d9a24496e9407b
maidanhcongtu@gmail.com <maidanhcongtu@gmail.com@27cc16b5-34d3-3ae7-c9fc-f95918a9f469>
2015-01-31 06:16:56 +0000

    init backlink init template

--- trunk/mytemplate/src/main/java/com/mhdanh/mytemplate/controller/AdminController.java
+++ trunk/mytemplate/src/main/java/com/mhdanh/mytemplate/controller/AdminController.java
@@ -2,6 +2,7 @@ package com.mhdanh.mytemplate.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -17,5 +18,10 @@ public class AdminController {
 	@ResponseBody
 	private String initAllTemplateFromZipFile(){
 		return adminService.initAllTemplateFromZipFile();
+	}
+	
+	@RequestMapping(value = {"/admin","admin/"})
+	private String indexAdmin(Model model){
+		return adminService.indexAdmin(model);
 	}
 }
--- trunk/mytemplate/src/main/java/com/mhdanh/mytemplate/service/implement/AdminServiceImpl.java
+++ trunk/mytemplate/src/main/java/com/mhdanh/mytemplate/service/implement/AdminServiceImpl.java
@@ -1,23 +1,75 @@
 package com.mhdanh.mytemplate.service.implement;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.util.List;
+
 import javax.transaction.Transactional;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
 
+import com.mhdanh.mytemplate.dao.TemplateDao;
+import com.mhdanh.mytemplate.domain.Template;
 import com.mhdanh.mytemplate.service.AdminService;
+import com.mhdanh.mytemplate.service.TemplateService;
+import com.mhdanh.mytemplate.service.UnzipService;
+import com.mhdanh.mytemplate.utility.Utility;
 
 @Service
 @Transactional
 public class AdminServiceImpl implements AdminService{
 	
-	@Autowired
+	private static final Logger logger = Logger.getLogger(AdminServiceImpl.class);
+	private final String FOLDER_ZIP_TEMPLATE = "system.url.store.template";
 	
+	
+	@Autowired
+	private TemplateDao templateDao;
+	@Autowired
+	private Utility utility;
+	@Autowired
+	private UnzipService unzipService;
 	
 	@Override
 	public String initAllTemplateFromZipFile() {
-		
-		return "Done";
+		try {
+			logger.warn("begin init all template from zip file");
+			List<Template> templates = templateDao.getAll();
+			for(Template template : templates){
+				//get zip file template
+				String pathZipFileTemplate = utility.getValueFromPropertiesSystemFile(FOLDER_ZIP_TEMPLATE)
+						+ utility.convertTextInDatabaseToNormalText(template.getCategory().getName())
+						+ "/" + template.getFileName();
+				logger.warn("input Template:" + pathZipFileTemplate);
+				File fileZipTemplate = new File(pathZipFileTemplate);
+				if(fileZipTemplate.exists()){
+					//file exist
+					String outPutTemplate = utility.getHtmlWebappPath()
+							+ utility.convertTextInDatabaseToNormalText(template.getCategory().getName())
+							+ "/"
+							+ utility.getNameWithouExtension(template.getFileName());
+					logger.warn("out Template:" + outPutTemplate);
+					// extract zip file to folder template
+					unzipService.unZip(pathZipFileTemplate, outPutTemplate);
+				}else{
+					//file note exist
+					logger.warn("file not exist:" + pathZipFileTemplate);
+				}
+			}
+			return "Done";
+		} catch (Exception e) {
+			logger.error("init template unsuccessful:",e);
+			return "error";
+		}
+	}
+
+	@Override
+	public String indexAdmin(Model model) {
+		logger.warn("index admin");
+		return "/admin";
 	}
 	
 }
